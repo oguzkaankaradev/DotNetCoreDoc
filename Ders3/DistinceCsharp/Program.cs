d85762d2c81c48b5172462c755d262d871ebcb80
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2024-11-03 21:37:56 +0300

    Add project files.

---
+++ Ders3/DistinceCsharp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+static async Task<int> GetPageLengthAsync(string endpont)
+{
+    var client = new HttpClient();
+    var uri = new Uri(endpont);
+    byte[] content = await client.GetByteArrayAsync(uri);
+    return content.Length;
+}
+
+
