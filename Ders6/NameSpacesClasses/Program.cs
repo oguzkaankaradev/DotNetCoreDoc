69d50c4e9552c7e2888d830aaa2b5b4d7180afc4
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2024-11-23 23:35:10 +0800

    İfElse

---
+++ Ders6/NameSpacesClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Namespace sayesinde biz yazdığımız kodu başka satfalarda kullanıyoruz.
+//using ile kullanacağımız namespaceyi 
+using Introduction;
+using payment;
+
+Console.WriteLine("Hello, World!");
+
+//bu alan siparişler alanı
+Order order = new Order();
+order.Name = "BAKLAVA";
+
+//Bu alan Ödeme alanı
+
+GarantiBankKartBilgileri garantiBankKartBilgileri = new GarantiBankKartBilgileri();
---
+++ IfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿//Şart sağlıyorsa işlem gerçekleşsin - eğer ; kısacası if koşulu sağlıyorsa yapılması gereknler
+//şart sağlamıyorsa gerçekleştirsin. - değilse ; koşulu sağlamıyorsa yapılamsı gerekenler
+int a = 5;
+int b = 3;
+
+if (a + b > 10)//sepet
+{
+    Console.WriteLine("hediye çeki kazandınız");
+}
+else
+{
+    Console.WriteLine("alışverişe devam et.");
+}
+
+
+//ikinci kullanımı 
+if (a + b > 10)//sepet
+    Console.WriteLine("hediye çeki kazandınız");
+else
+    Console.WriteLine("alışverişe devam et.");
+
+//
