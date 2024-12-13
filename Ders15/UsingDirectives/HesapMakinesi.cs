cfc6bd19322660da7e627ec4a5c338d961672c0b
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2024-12-14 00:50:59 +0800

    Using

---
+++ Ders15/UsingDirectives/HesapMakinesi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    public class HesapMakinesi
+    {
+        public void Hesaplama()
+        {
+            
+            //hesap makinesi.
+            Console.WriteLine("Hesaplama yapıldı.");
+        }
+    }
+}
